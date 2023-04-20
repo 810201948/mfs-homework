d3be718fb2d1b2f14c71cae194cb5e26581bca5b
810201948 <121553120+810201948@users.noreply.github.com>
2023-04-20 14:08:16 +0800

    Add files via upload

---
+++ webpack/single/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>{{sum}}</div>
+    <my-btn @inc="inc"></my-btn>
+    <my-btn @inc="inc"></my-btn>
+    <my-btn @inc="inc"></my-btn>
+  </div>
+
+  
+  
+</template>
+
+<script>
+import MyBtn from './MyBtn'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      sum:0
+    }
+  },
+  methods:{
+    inc(){
+      this.sum++
+    }
+
+  },
+  components:{MyBtn}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
