b70d31f32f6cca4c7db886beae47540b79baa6a8
810201948 <121553120+810201948@users.noreply.github.com>
2023-04-20 19:41:10 +0800

    Add files via upload

---
+++ webpack/vue-router/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <router-link to="/">home</router-link>
+        <router-link to="/about">about</router-link>
+        <p>this is home page</p>
+    </div>
+</template>
+
+<script>
+    import VLink from './VLink'
+    export default{
+        components:{
+            VLink
+        }
+    }
+</script>
+
+<style>
+
+</style>
