e004e41e34e2b3a60f7718176a5351a343e7d3c7
810201948 <121553120+810201948@users.noreply.github.com>
2023-04-30 20:48:53 +0800

    Add files via upload

---
+++ vue-blog/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" class="container">
+    <main-nav>
+    </main-nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MainNav from './components/MainNav'
+export default {
+  components:{MainNav},
+  name: 'App'
+}
+</script>
+
+<style>
+  @import '../src/assets/css/base.css';
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  ol,ul,body{
+    margin:0;
+    padding:0;
+  }
+  ol,ul{
+    list-style:none;
+  }
+  .container{
+    width:80%;
+    margin:0 auto;
+  }
+</style>
