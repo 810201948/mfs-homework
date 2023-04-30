582c6121e5a89bf81b6f739b0a216572eae7302c
810201948 <121553120+810201948@users.noreply.github.com>
2023-04-30 20:43:07 +0800

    Create About.vue

---
+++ vue-blog/src/pages/About.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about">
+        <div class="title">
+            About Me
+        </div>
+        <div class="logo">
+          <img src="../assets/logo.png" alt="">
+        </div>
+    <div class="content">
+      <p>Responsive web design offers us a way forward, finally allowing us to design for the ebb and flow of things. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly.</p>
+      <p>Responsive web design offers us a way forward, finally allowing us to design for the ebb and flow of things. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly.</p>
+    </div>
+  </div>
+  
+    
+
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title{
+  text-align: center;
+  font-size: 36px;
+  font-weight: 700;
+}
+.logo{
+  margin-top: 30px;
+  text-align: center;
+}
+.content{
+  font-size: 20px;
+  line-height: 150%;
+  margin: 30px 0 100px 0;
+}
+</style>
